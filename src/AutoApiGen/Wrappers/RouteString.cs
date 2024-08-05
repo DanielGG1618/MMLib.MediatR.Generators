d09fd3bf540adef336d1308cdfaf838a19f10151
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-05 21:10:39 +0500

    feat: created route string wrapper

---
+++ src/AutoApiGen/Wrappers/RouteString.cs
@@ -0,0 +1,19 @@
+﻿namespace AutoApiGen.Wrappers;
+
+public class RouteString
+{
+    private readonly string _string;
+    private readonly IImmutableList<string> _parts;
+
+    public string BaseRoute => _parts[0];
+    
+    public static RouteString Wrap(string @string) => new(@string);
+    
+    public static implicit operator string(RouteString routeString) => routeString._string;
+    
+    private RouteString(string @string)
+    {
+        _string = @string;
+        _parts = _string.Split('/').ToImmutableArray();
+    }
+}
